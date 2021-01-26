211b4fac75edff42e9177dea5ccb9ee5dd27f241
BIT Instructor <bitinstructor@bitnet.wctc.edu>
2021-01-25 19:20:36 -0600

    Added TODO

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ namespace Class_2
 {
     public class Program
     {
+        // TODO: Update in T1234
         static void Main(string[] args)
         {
             Console.WriteLine("Hola a todos!");
