8b9b6261ba892b857930ec5c4790bd918840a5ed
BIT Instructor <bitinstructor@bitnet.wctc.edu>
2021-01-25 18:58:31 -0600

    Updated language to spanish

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace Class_2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola a todos!");
         }
     }
 }
